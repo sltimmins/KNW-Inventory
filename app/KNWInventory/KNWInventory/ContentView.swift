17e8d878c9e77e01e0a01f0f8ff2fcf28cffd93c
leongkkevin <kkleong@smu.edu>
2021-06-11 13:59:33 -0700

    App Stuff

---
+++ app/KNWInventory/KNWInventory/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  KNWInventory
+//
+//  Created by Kevin Leong on 6/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 12")
+    }
+}
---
+++ app/KNWInventory/KNWInventory/KNWInventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  KNWInventoryApp.swift
+//  KNWInventory
+//
+//  Created by Kevin Leong on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct KNWInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
